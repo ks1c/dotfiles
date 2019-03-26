5c4401f0317bc2d9e52ea2161afadaa6fb030e82
ks1c <ksic3784@gmail.com>
2019-03-26 09:44:14 -0400

    custom prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,13 @@
 function fish_prompt
-	echo -n '$ '
+    set_color purple
+    echo -n [
+    set_color normal
+    echo -n $USER' ' 
+    if test "$PWD" != "$HOME"    
+        echo -n (basename $PWD)
+    else 
+        echo -n '~'
+    end
+    set_color purple
+    echo -n '] '
 end
