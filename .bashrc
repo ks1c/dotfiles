27479b1988cc395e35c6774e698c149c9a0e8f01
ks1c <ksic3784@gmail.com>
2019-06-17 22:41:10 -0400

    remove aliases for vifm script

--- .bashrc
+++ .bashrc
@@ -2,7 +2,6 @@
 
 alias ls='ls --color=auto'
 alias vim='nvim'
-alias vifm='$HOME/scripts/vu.sh'
 
 PS1='[\u@\h \W]\$ '
 
--- fish/functions/vifm.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function vifm --description 'alias vifm vu.sh'
-	$HOME/scripts/vu.sh  $argv;
-end
